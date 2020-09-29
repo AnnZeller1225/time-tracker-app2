18f63e749e85d1fa7e83a1503022588b0c431d8b
AnnZeller <annazeller@mail.ru>
2020-09-29 11:18:07 +0300

    Updates

---
+++ precache-manifest.886b605233942c957a8e683101c60012.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33bca686ff82060afbe4f89a6b089e5",
+    "url": "/time-tracker/index.html"
+  },
+  {
+    "revision": "213460d56641f0219d96",
+    "url": "/time-tracker/static/css/main.6b8db9aa.chunk.css"
+  },
+  {
+    "revision": "547889071d6bebf7f9d0",
+    "url": "/time-tracker/static/js/2.4efe9ab7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/time-tracker/static/js/2.4efe9ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213460d56641f0219d96",
+    "url": "/time-tracker/static/js/main.6b635542.chunk.js"
+  },
+  {
+    "revision": "cba10c87347a6f1e5893",
+    "url": "/time-tracker/static/js/runtime-main.591cc97a.js"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/time-tracker/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/time-tracker/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "2786ee486e9d789a9f2ff04d3b4cd5fd",
+    "url": "/time-tracker/static/media/Vector (Stroke).2786ee48.svg"
+  },
+  {
+    "revision": "a2f51fff44bead6554e15c5f38b6df05",
+    "url": "/time-tracker/static/media/play.a2f51fff.svg"
+  },
+  {
+    "revision": "079d28e6fee93e021cda4e8dbe2f1892",
+    "url": "/time-tracker/static/media/plus.079d28e6.svg"
+  },
+  {
+    "revision": "138d4892c059b2750b5959132f9d65cd",
+    "url": "/time-tracker/static/media/x.138d4892.svg"
+  }
+]);
